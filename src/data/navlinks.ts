4de01869d51022e9491ad37c26888baf8ab386a9
Churtado26 <tobiitas2009@gmail.com>
2026-01-13 17:19:16 -0500

    Initial commit - Aya Cosméticos landing base

---
+++ src/data/navlinks.ts
@@ -0,0 +1,8 @@
+import type { INavLink } from "../types";
+
+export const navlinks = [
+    { name: "Inicio", href: "#" },
+    { name: "Productos", href: "#productos" },
+    { name: "Nosotros", href: "#nosotros" },
+    { name: "Contacto", href: "#contacto" },
+];
